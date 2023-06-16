75ce3c8a4b56cb1ae5fbe7a9c7796a59fdf40a38
Yugathasan-Varsha <136677081+Yugathasan-Varsha@users.noreply.github.com>
2023-06-16 13:49:59 +0530

    Add files via upload

---
+++ my first program 3.py
@@ -0,0 +1,2 @@
+print('Hello World!')
+person=input('What is your name?')
---
+++ my first program 5.py
@@ -0,0 +1,9 @@
+print('Hello World!')
+person=input('What is your name?')
+print('Hello',person)
+
+
+
+age=14
+print(age)
+14
